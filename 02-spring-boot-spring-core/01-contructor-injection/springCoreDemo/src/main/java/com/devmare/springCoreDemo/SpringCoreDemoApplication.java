2c300d0a5db3ebf8bb0dab7545a9ada0d20f7193
Ankush <ankushpaulclg2002@gmail.com>
2023-08-08 20:45:25 +0530

    second commit

---
+++ 02-spring-boot-spring-core/01-contructor-injection/springCoreDemo/src/main/java/com/devmare/springCoreDemo/SpringCoreDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devmare.springCoreDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreDemoApplication.class, args);
+	}
+
+}
