146c92b67fe7ca2cbc1f3f6733a488af72cc7e7d
Ankush <ankushpaulclg2002@gmail.com>
2023-08-12 22:25:20 +0530

    2.6 commit

---
+++ 02-spring-boot-spring-core/06-lazy-init/springCoreDemo/src/main/java/com/devmare/springCoreDemo/SpringCoreDemoApplication.java
@@ -0,0 +1,12 @@
+package com.devmare.springCoreDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringCoreDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreDemoApplication.class, args);
+    }
+}
