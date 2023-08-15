a0fad5d1edd841416fea83663cca27799f023f14
Ankush <ankushpaulclg2002@gmail.com>
2023-08-15 16:51:40 +0530

    3.1 commit

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/devmare/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/devmare/cruddemo/CruddemoApplication.java
@@ -1,13 +1,21 @@
 package com.devmare.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return runner -> {
+            System.out.println("Hello Ankush");
+        };
+    }
 }
