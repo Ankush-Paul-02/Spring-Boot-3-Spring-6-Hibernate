86bb8aebc9f1f87e0e44373ff857e78995cef293
Ankush <ankushpaulclg2002@gmail.com>
2023-08-24 02:47:49 +0530

    4.4 spring data jpa and spring rest jpa

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-spring-data-rest/src/main/java/com/devmare/demo/repository/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-spring-data-rest/src/main/java/com/devmare/demo/repository/EmployeeRepository.java
@@ -3,4 +3,7 @@ package com.devmare.demo.repository;
 import com.devmare.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+//? @RepositoryRestResource(path = "members")
+//? http://localhost:8081/devmare-api/members
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {}
