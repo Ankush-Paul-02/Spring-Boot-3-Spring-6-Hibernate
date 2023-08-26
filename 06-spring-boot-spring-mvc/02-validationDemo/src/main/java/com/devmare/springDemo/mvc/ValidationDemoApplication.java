ae85f4cb7b8afa4f9c8d51eb124d0e108b3ffd94
Ankush <ankushpaulclg2002@gmail.com>
2023-08-27 02:21:24 +0530

    6.2 commit

---
+++ 06-spring-boot-spring-mvc/02-validationDemo/src/main/java/com/devmare/springDemo/mvc/ValidationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devmare.springDemo.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationDemoApplication.class, args);
+    }
+
+}
---
+++ 06-spring-boot-spring-mvc/02-validationDemo/src/test/java/com/devmare/springDemo/mvc/ValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmare.springDemo.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
