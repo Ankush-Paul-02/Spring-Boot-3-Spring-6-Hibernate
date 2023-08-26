939f1e622ba450ab2da472a64475adc8592e106f
Ankush <ankushpaulclg2002@gmail.com>
2023-08-27 01:28:35 +0530

    6 commit

---
+++ 06-spring-boot-spring-mvc/01-thymeLeaf/src/main/java/com/devmare/demo/ThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package com.devmare.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeLeafApplication.class, args);
+    }
+
+}
