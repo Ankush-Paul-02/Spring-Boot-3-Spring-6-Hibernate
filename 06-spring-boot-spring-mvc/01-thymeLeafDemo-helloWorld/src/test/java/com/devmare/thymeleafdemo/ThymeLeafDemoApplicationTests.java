b6f8fd72b3a45401b118114f290a43047e3ead76
Ankush <ankushpaulclg2002@gmail.com>
2023-08-27 01:13:49 +0530

    06 commit

---
+++ 06-spring-boot-spring-mvc/01-thymeLeafDemo-helloWorld/src/test/java/com/devmare/thymeleafdemo/ThymeLeafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmare.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeLeafDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
