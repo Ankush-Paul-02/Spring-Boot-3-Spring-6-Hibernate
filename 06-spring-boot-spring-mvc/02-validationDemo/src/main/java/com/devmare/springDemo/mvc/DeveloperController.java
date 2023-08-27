c7d7a998c9ee6ac8f1b5efe233428013d7865817
Ankush <ankushpaulclg2002@gmail.com>
2023-08-27 16:37:49 +0530

    6.2 Spring Boot - Spring MVC Validation - @InitBinder

--- 06-spring-boot-spring-mvc/02-validationDemo/src/main/java/com/devmare/springDemo/mvc/DeveloperController.java
+++ 06-spring-boot-spring-mvc/02-validationDemo/src/main/java/com/devmare/springDemo/mvc/DeveloperController.java
@@ -1,15 +1,28 @@
 package com.devmare.springDemo.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DeveloperController {
+
+    //! Add an init binder ... to convert trim input strings
+    //! Remove leading and trailing whitespaces
+    //! Resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
     @GetMapping("/")
     public String showForm(Model model) {
         model.addAttribute("developer", new Developer());
@@ -20,6 +33,7 @@ public class DeveloperController {
     public String processFrom(
             @Valid @ModelAttribute("developer") Developer developer,
             BindingResult bindingResult) {
+        System.out.println("Last name: | " + developer.getLastName() + " |");
         if (bindingResult.hasErrors()) {
             return "developer-form";
         } else {
