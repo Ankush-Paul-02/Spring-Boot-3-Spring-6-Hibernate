75c019ff418dcbd7fe18193897806cb76fb41b88
Ankush <ankushpaulclg2002@gmail.com>
2023-08-31 03:25:00 +0530

    8.1 Spring MVC Security - Login Form with Error Message

---
+++ 08-spring-boot-spring-mvc-sequrity/01-spring-boot-spring-mvc-sequrity-default/src/main/java/com/devmare/demo/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.devmare.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+        return "login-page";
+    }
+}
---
+++ 08-spring-boot-spring-mvc-sequrity/01-spring-boot-spring-mvc-sequrity-default/src/main/java/com/devmare/demo/security/DemoSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.devmare.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails ankush = User.builder()
+                .username("ankush")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+        UserDetails sid = User.builder()
+                .username("sid")
+                .password("{noop}test123")
+                .roles("EMPLOYEE, MANAGER")
+                .build();
+        UserDetails deepon = User.builder()
+                .username("deepon")
+                .password("{noop}test123")
+                .roles("EMPLOYEE, MANAGER, ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(ankush, sid, deepon);
+    }
+
+    //! Config our spring security, Reference of our custom login form
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configure ->
+                        configure
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser") //? No controller request mapping required for this process url. We got this for free
+                                .permitAll()
+                );
+        return httpSecurity.build();
+    }
+}
