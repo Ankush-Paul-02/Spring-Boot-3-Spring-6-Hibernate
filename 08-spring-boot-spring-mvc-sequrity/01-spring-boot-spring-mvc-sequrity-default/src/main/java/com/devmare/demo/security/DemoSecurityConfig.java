8d83678ee99878b833adec7d34c0cb0d008c1cfc
Ankush <ankushpaulclg2002@gmail.com>
2023-08-31 21:31:45 +0530

    8.1 Spring MVC Security - Logout

--- 08-spring-boot-spring-mvc-sequrity/01-spring-boot-spring-mvc-sequrity-default/src/main/java/com/devmare/demo/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-sequrity/01-spring-boot-spring-mvc-sequrity-default/src/main/java/com/devmare/demo/security/DemoSecurityConfig.java
@@ -3,6 +3,7 @@ package com.devmare.demo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -42,6 +43,8 @@ public class DemoSecurityConfig {
                                 .loginPage("/showMyLoginPage")
                                 .loginProcessingUrl("/authenticateTheUser") //? No controller request mapping required for this process url. We got this for free
                                 .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll
                 );
         return httpSecurity.build();
     }
