1fede7d257f0e044687d39e942a6222a39ff8e4e
Ankush <ankushpaulclg2002@gmail.com>
2023-09-02 16:16:34 +0530

    9.1 commit

--- 09-spring-boot-jpa-advanced-mapping/01-jpa-one-to-one-uni/src/main/java/com/devmare/cruddemo/CrudDemoApplication.java
+++ 09-spring-boot-jpa-advanced-mapping/01-jpa-one-to-one-uni/src/main/java/com/devmare/cruddemo/CrudDemoApplication.java
@@ -1,13 +1,13 @@
-package com.devmare.springauth;
+package com.devmare.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAuthApplication {
+public class CrudDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringAuthApplication.class, args);
+        SpringApplication.run(CrudDemoApplication.class, args);
     }
 
 }
